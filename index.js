12ebeede8489a3cfafdf1a9969c997ed3d1b5d32
kanny <kanny.ghafour87@gmail.com>
2023-06-11 21:52:51 -0500

    lab test completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        left--;
+        dodger.style.left = `${left}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+
+    const gameWidth = 400; // Adjust this value according to your game width
+
+    if (left < gameWidth - dodger.offsetWidth) {
+        left++;
+        dodger.style.left = `${left}px`;
+    }
+}
